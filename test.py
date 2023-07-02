f706976dc0838ae3c25d3013db62f751a2c89f76
Kontuch Suksawat <113300889+Fishcanwalk@users.noreply.github.com>
2023-07-02 11:02:50 +0700

    Update test.py

--- test.py
+++ test.py
@@ -6,33 +6,8 @@ import tempfile
 import os
 import os.path as osp
 
-st.title('Deployment Ai builder')
-st.title('object-detection-for-CCTV-with-traffic')
-with st.sidebar:
-  st.title("Option")
-  option = st.selectbox('How would you like to be contacted?',('Image', 'Video'))
-  st.title("Select_Model")
-  option = st.selectbox('How would you like to be contacted?',('Model-v5', 'Model-v8')) 
-    
-if option == 'Video' :
-  st.write('Using video upload option')
-else :
-  st.write('Using image upload option')
-    
-if option == 'Image':
-  if Select_Model == 'Model-v5':
-    model = torch.hub.load('ultralytics/yolov5', 'custom',
+model = torch.hub.load('ultralytics/yolov5', 'custom',
                            path='mymodelv5.pt', force_reload=True)
-  else:
-    model = YOLO('my_modeln.pt')
-  ImgPre(model) 
-else :
-  if Select_Model == 'Model-v5':
-    model = torch.hub.load('ultralytics/yolov5', 'custom',
-                           path='mymodelv5.pt', force_reload=True)
-  else:
-    model = YOLO('my_modeln.pt')
-  videoPre(model)
 
 def ImgPre(m) :
   image_file = st.file_uploader("Upload An Image", type=['png', 'jpeg', 'jpg'])
@@ -70,15 +45,14 @@ def videoPre (m):
               if success:
                 frame_count += 1
                 res = m(image)
-                result_tensor = res[0].boxestorch.hub.load('ultralytics/yolov5', 'custom',
-                           path=CFG_MODEL_PATH, force_reload=True, device=device)
+                result_tensor = res[0].boxes
                 res_plotted = res[0].plot()
                 im = Image.fromarray(res_plotted[:,:,::-1])
                 st_frame.image(res_plotted,
                                caption='Detected Video',
                                channels="BGR",
                                use_column_width=True
-                               )Error: This app has encountered an error. The original error message is redacted to prevent data leaks. Full error details have been recorded in the logs (if you're on Streamlit Cloud, click on 'Manage app' in the lower right of your app).
+                               )
                 im.save(osp.join(frames_dir, f'{frame_count}.jpg'))
               else :
                  vid_cap.release()
@@ -88,8 +62,20 @@ def videoPre (m):
             os.system(f'rm -rf {frames_dir}')
             output_video = open(outputpath, 'rb')
             output_video_bytes = output_video.read()
-            st.video(output_video_bytes)
-        
-
-    
-
+            st.video(output_video_bytes)      
+def main() :
+  st.title('Deployment Ai builder')
+  st.title('object-detection-for-CCTV-with-traffic')
+  with st.sidebar:
+    st.title("Option")
+    option = st.selectbox('How would you like to be contacted?',('Image', 'Video'))
+  if option == 'Video' :
+    st.write('Using video upload option')
+  else :
+    st.write('Using image upload option')
+  if option == 'Image':
+    ImgPre(model) 
+  else :
+    videoPre(model)
+if __name__ == "__main__":
+  main()
